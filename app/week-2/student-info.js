9347ee5bb426e07e3c31411dd341de9debd581b1
jincabia <140627796+jincabia@users.noreply.github.com>
2024-01-08 15:39:11 -0700

    week-2
    Finish week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export function StudentInfo() {
+    return (
+      
+        <div>
+        <h1>Jin Francis Cabia</h1>
+        <Link href="https://github.com/jincabia/cprg306-assignments">Github repo</Link>
+        </div>
+      
+    );
+  }
+
+ 
