abd3b9a01b787d996c8d28019d54ec139e58ca36
jincabia <140627796+jincabia@users.noreply.github.com>
2024-01-20 13:01:44 -0700

    Finished week-4

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,74 +1,95 @@
 'use client';
 import { useState } from 'react';
 
-export function NewItem()
-{
-    const[name, setName] = useState("")
-    const[quantity, setQuantity] = useState(1)
-    const[category, setCategory] = useState("")
 
 
 
+export function NewItem() {
 
-                /*Create an item object with the current values of name, quantity, and category.
-                Log the item object to the console.
-                Display an alert with the current state of name, quantity, and category.
-                Reset the state variables to their initial values.*/ 
+    const [name,setName] = useState("");
+    const [quantity,setQuantity] = useState(1);
+    const [category,setCategory] = useState("Produce");
 
-                // const item12 = {
-                //     name: "hand soap 🧼",
-                //     quantity: 4,
-                //     category: "household",
-                //   };
-    const submitFunction = (event) => {
+    const handleSubmit = event => {
         event.preventDefault();
-        const newItem = {
-            Name: name,
-            Quantity: quantity,
-            Category: category,
-            
-        };
-        console.log(newItem);
-        alert(name,quantity,category);
+        const item = {name,quantity,category};
+        console.log(item);
+        alert(`Added item: ${name}, quantity: ${quantity}, category: ${category}`);
+        //Added item: a, quantity: 1, category: produce
+        setName("");
+        setQuantity(1);
+        setCategory("");
         
-      };
+    };
+    
+    const changeName = event => {
+        console.log(event.target.value);
+        setName(event.target.value);
+    };
 
-   
-    return(
-        <div>
-            <form onSubmit="submitFunction(event)">
-            <input type="text" name="name" placeholder="Enter your name" className='text-black' />
-            <input type="number" name="quantity" placeholder="Enter your Quantity" className='text-black' />
-            <input type="text" name="category" placeholder="Enter your Category" className='text-black' />
-            <button type="submit">Submit</button>
-            </form>
+    const changeQuantity = event => {
+        console.log(event.target.value);
+        
+        setQuantity(event.target.value);
+    };
 
-
-            
-
-            {/* Change Name: <input value={name} onChange={n=> setName(n.target.value)}/>
-            <p>Name: {name}</p> */}
-
-
-        </div>
-    )
-
-}
+    const changeCategory = event => {
+        console.log(event.target.value);
+        setCategory(event.target.value);    
+    };
 
 
-// export function CounterExample() {
-//     const [count, setCount] = useState(0);
-   
-//     const increment = () => {
-//       setCount(count + 1);
-//     };
-   
-//     return (
-//       <div>
-//         <p>Count: {count}</p>
-//         <button onClick={increment}>Increment</button>
-//       </div>
-//     );
-//   }
 
+ 
+        return (
+            <div className='flex items-center justify-center'>
+                <div>
+                    <form onSubmit={handleSubmit} className='bg-gray-500 m-4 p-4 grid grid-rows-2 grid-flow-row gap-4 rounded'>
+                        <input
+                            required
+                            onChange={changeName}
+                            value={name}
+                            className='text-black mb-4 w-max p-4'
+                            placeholder='Enter Item Name'
+                        />
+                        <input
+                            required
+                            max="99"
+                            onChange={changeQuantity}
+                            type='number'
+                            value={quantity}
+                            className='text-black mb-4 pl-4 rounded'
+                        />
+    
+                        <select
+                            onChange={changeCategory}
+                            value={category}
+                            className='text-black p-4 rounded'
+                            placeholder='Enter Item Category'
+                        >
+                            <option disabled>Category</option>
+                            <option value="Produce">Produce</option>
+                            <option value="Dairy">Dairy</option>
+                            <option value="Bakery">Bakery</option>
+                            <option value="Meat">Meat</option>
+                            <option value="Frozen Foods">Frozen Foods</option>
+                            <option value="Canned Goods">Canned Goods</option>
+                            <option value="Dry Goods">Dry Goods</option>
+                            <option value="Beverages">Beverages</option>
+                            <option value="Snacks">Snacks</option>
+                            <option value="Household">Household</option>
+                            <option value="Other">Other</option>
+                        </select>
+    
+                        <div className='flex items-center justify-center'>
+                            <button className='py-3 px-20 bg-blue-500 text-white rounded hover:bg-blue-700'>
+                                +
+                            </button>
+                        </div>
+    
+                    </form>
+                </div>
+            </div>
+        );
+  }
 
